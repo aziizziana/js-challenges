35ed9b27af1ed83e9c2073923cce82ff065fd52b
Ziana <aziizziana@gmail.com>
2019-09-21 11:45:16 +0300

    define age

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -7,5 +7,5 @@ module.exports = fizzBuzz;*/
 for (var i=1; i < 50; i++){
     if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    else console.log(i); 
 }
